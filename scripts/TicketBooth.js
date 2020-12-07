141ce1bf26c86db15fd52db25ec8da2ba18b2023
Xine75 <cjdoza@gmail.com>
2020-12-07 11:59:34 -0600

    ride tickets

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -1,7 +1,23 @@
+//Get a reference ot the DOM element where the <button> will be rendered
 const contentTarget = document.querySelector(".entry")
+//setting up eventHub, naming variable, declaring where it will call from the DOM
 const eventHub = document.querySelector("#state-fair")
 
-eventHub.addEventListener()
+//Create custom message via eventHub. This doesn't run until click event is triggered
+eventHub.addEventListener("click", event => {
+
+//Check to see if ride button was clicked. "rideTicket" must match button id on line 25
+    if (event.target.id === "rideTicket") {
+//Create customEvent
+        const customEvent = new CustomEvent ("rideClicked", {
+            detail: {
+                rideButton: event.target.value
+            }
+        })
+//Dispatch (broadcast) event to eventHub
+        eventHub.dispatchEvent(customEvent)
+    }
+})
 
 export const TicketBooth = () => {
     contentTarget.innerHTML = `
--- scripts/rides/RideTicketHolders.js
+++ scripts/rides/RideTicketHolders.js
@@ -1,9 +1,21 @@
+//Defines where the number of rides will appear in DOM
 const contentTarget = document.querySelector(".rides")
+//defines where to listen for eventHub broadcast
 const eventHub = document.querySelector("#state-fair")
 
-export const RideTicketHolders = () => {
-    eventHub.addEventListener("rideEvent", customEvent => {
 
+export const RideTicketHolders = () => {
+    
+    eventHub.addEventListener("rideClicked", event => {
+            console.log ("Im listening")
+            contentTarget.innerHTML +=  `
+            <div class="person rider"></div>
+            `
+
+
+
+        
+       
     })
 }
 
